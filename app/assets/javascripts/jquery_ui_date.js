e34d94801b1a5f9cf97d415edede3c74837fda53
David Tsang <davidytsang@gmail.com>
2015-04-29 13:02:13 -0400

    styling

--- app/assets/javascripts/jquery_ui_date.js
+++ app/assets/javascripts/jquery_ui_date.js
@@ -1,7 +1,2 @@
 //= require jquery-ui/datepicker
 //= require jquery.timepicker.js
-
-$(function() {
-  $('.datepicker').datepicker({ dateFormat: 'dd-mm-yy' }).val();
-  $('.timepicker').timepicker();;
-});
